ba6d6f77970b51ac3765787af038792468192bfb
konrad101296 <konradslimak@o2.pl>
2024-03-02 19:12:23 +0100

    Add project files.

---
+++ week 2 lesson 8 exercise nr.1/Program.cs
@@ -0,0 +1,19 @@
+﻿int liczbapierwsza = 0;
+int reszta = 0;
+for (int i = 2; i <= 100; i++)
+{
+
+    for (int j = 1; j <= i; j++)
+    {
+        if (i % j == 0)
+        {
+            reszta++;
+        }
+        if (reszta == 2 && i == j)
+        {
+            liczbapierwsza++;
+        }
+    }
+    reszta = 0;
+}
+Console.WriteLine($"W zakresie od 0 do 100 jest {liczbapierwsza} liczb pierwszych");
